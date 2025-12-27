8103d28bf6b047b34c541450c0607b3a851a01d0
Jordy van Domselaar <jordy@jordy.app>
2025-12-27 07:19:08 +0100

    Add sitemap integration

--- packages/site/astro.config.mjs
+++ packages/site/astro.config.mjs
@@ -2,11 +2,13 @@
 import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  site: process.env.SITE_URL ?? 'https://nrftw.jordy.app/',
+  integrations: [react(), sitemap()],
 
   vite: {
     plugins: [tailwindcss()]
